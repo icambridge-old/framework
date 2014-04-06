a7fabc11bb0043cffd784cdd36a956531dbe248d
Iain Cambridge <github@icambridge.me>
2014-04-06 15:09:45 +0100

    Quick refactor

---
+++ app.go
@@ -0,0 +1,25 @@
+package framework
+
+import (
+	"reflect"
+)
+
+type App struct {
+	controllers  map[string]reflect.Type
+
+	router Router
+}
+
+func (a *App) RegisterController(c interface {}) {
+	t := reflect.TypeOf(c)
+	a.controllers[t.Name()] = t
+}
+
+func (a *App) RegisterRouter(r Router) {
+	a.router = r
+}
+
+func NewApp() *App {
+
+	return &App{controllers: map[string]reflect.Type{}}
+}
---
+++ app_test.go
@@ -0,0 +1,42 @@
+package framework
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestApp_RegisterController(t *testing.T) {
+
+	type TestController struct {
+		Controller
+	}
+
+	a := NewApp()
+	a.RegisterController(TestController{})
+
+	if _, ok := a.controllers["TestController"]; ok == false {
+		t.Error("Expected TestController to be registered it wasn't")
+	}
+
+}
+
+func TestNewApp(t *testing.T) {
+
+	a := NewApp()
+
+	if len(a.controllers) != 0 {
+		t.Errorf("Expected no controllers, %v found", len(a.controllers))
+	}
+}
+
+func TestApp_RegisterRouter(t *testing.T) {
+
+	r := Router{}
+
+	a := NewApp()
+	a.RegisterRouter(r)
+
+	if !reflect.DeepEqual(r,a.router) {
+		t.Errorf("Router didn't match, expected %v got %v", r, a.router)
+	}
+}
